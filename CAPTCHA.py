08a165a320d7be8e11f047da13f060b2f496f22d
Will <will.hingston@gmail.com>
2018-06-26 14:41:05 +0930

    Added captcha completer

---
+++ CAPTCHA.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from multiprocessing.pool import ThreadPool
+
+s = requests.Session()
+solved_captchas = []
+
+
+def get_captchas(num):
+    for i in range(num):
+        r = s.get('http://43.241.202.33:3003/rest/captcha/', cookies={'from-my': 'browser'})
+        response = json.loads(r.text)
+        data = {"captcha": str(eval(response['captcha'])), "comment": "zzz", "rating": 3,
+                "captchaId": str(response['captchaId'])}
+        solved_captchas.append(json.dumps(data))
+
+
+def submit_captchas(data):
+    headers = {'Content-type': 'application/json'}
+    r = s.post('http://43.241.202.33:3003/api/Feedbacks/',
+               data=data,
+               headers=headers,
+               )
+    print(data)
+    print(r)
+
+
+get_captchas(10)
+
+with ThreadPool(len(solved_captchas)) as p:
+    p.map(submit_captchas, solved_captchas)
