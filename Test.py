29e8d0040ddd9a835636c7dc5235699872a5c4af
hingston <will.hingston@gmail.com>
2018-07-06 16:39:48 +0930

    Deleted

--- Test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-r = requests.get("http://43.241.202.33:3003/rest/product/search?q=TEST", timeout=10)
-
-print(r, r.text)
-
-r = requests.get("http://43.241.202.33:3003/rest/product/search?q='))--", timeout=10)
-
-print(r, r.text)
